95833fd125b21551abffd92962ab3602597b86e7
VictorShurapov <vshurapov@gmail.com>
2016-06-01 01:02:29 +0300

    Initial commit

---
+++ ShowSizeClass/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShowSizeClass
+//
+//  Created by Victor Shurapov on 5/24/16.
+//  Copyright © 2016 Victor Shurapov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
